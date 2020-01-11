143c50736a00987a205c5f281c7150bd00ae50f9
dompazdzierz <pazdzierzdominik@protonmail.com>
2020-01-11 19:28:47 +0100

    UncompletedTrips - pagination added

--- e-got-frontend/src/Views/UncompletedTrips/UncompletedTripsView.js
+++ e-got-frontend/src/Views/UncompletedTrips/UncompletedTripsView.js
@@ -1,52 +1,114 @@
 import React from 'react';
-import { Segment, Table } from 'semantic-ui-react';
+import './UncompletedTripsView.css';
+import { Segment, Table, Button, Menu, Icon, Pagination } from 'semantic-ui-react';
 import AppHeader from '../../Components/AppHeader/AppHeader';
 
 class UncompletedTripsView extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            currentPage: 1
         }
     }
 
+    handlePaginationChange = (e, { activePage }) =>  {
+        this.setState({ currentPage: activePage })
+        console.log(this.state.currentPage)
+    }
 
     render() {
 
         const trips = [
             { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
+            { route: 'Wycieczka 1: Palenica Białczańska - Wodogrzmoty Mickiewicza', date: '17.08.2019', points: '18' },
         ]
 
-        let containerStyle = {
-            textAlign: 'center'
-        }
+        let tripsNumber = trips.length
 
-        let segmentStyle = {
-            height: '80vh',
-            width: '80vw',
-            display: 'inline-block'
-        }
+
+        let tableContent =
+        trips.slice(0 + (this.state.currentPage - 1) * 8, 7 + (this.state.currentPage - 1) * 8).map((trip) => (
+            <Table.Row key={trip.route}>
+                <Table.Cell>{trip.route}</Table.Cell>
+                <Table.Cell>{trip.date}</Table.Cell>
+                <Table.Cell>{trip.points}</Table.Cell>
+            </Table.Row>
+        ))
 
         return(
-            <div style={containerStyle}>
+            <div className="uncompletedtrips--container">
                 <AppHeader />
-                <Segment style={segmentStyle}>
-                    <Table>
+                <Segment className="uncompletedtrips--segment">
+                    <Button primary content="Powrót" floated="left" className="uncompletedtrips--button"/>
+                    <Table className="uncompletedtrips--table">
                         <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Nazwa wycieczki</Table.HeaderCell>
-                            <Table.HeaderCell>Data</Table.HeaderCell>
-                            <Table.HeaderCell>Punktacja</Table.HeaderCell>
-                        </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                        {trips.map((trip) => (
-                            <Table.Row key={trip.route}>
-                                <Table.Cell>{trip.route}</Table.Cell>
-                                <Table.Cell>{trip.date}</Table.Cell>
-                                <Table.Cell>{trip.points}</Table.Cell>
+                            <Table.Row>
+                                <Table.HeaderCell>Nazwa wycieczki</Table.HeaderCell>
+                                <Table.HeaderCell>Data</Table.HeaderCell>
+                                <Table.HeaderCell>Punktacja</Table.HeaderCell>
                             </Table.Row>
-                        ))}
+                        </Table.Header>
+
+                        <Table.Body>
+                            {tableContent}
                         </Table.Body>
+
+                        <Table.Footer>
+                            <Table.Row>
+                                <Table.HeaderCell colSpan='3' style={{textAlign: 'center'}}>
+                                    <Pagination defaultActivePage={1} totalPages={Math.ceil(tripsNumber/8)}
+                                    boundaryRange={1} onPageChange={this.handlePaginationChange}/>
+                                </Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Footer>
                     </Table>
                 </Segment>
             </div>
