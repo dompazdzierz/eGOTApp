0f6f36a058117c9ace3bd551341e074992852e70
Karol Belina <karolbelina@gmail.com>
2020-01-18 09:47:00 +0100

    Add the section controller

---
+++ e-got-backend/eGOTBackend/Controllers/SectionController.cs
@@ -0,0 +1,59 @@
+﻿// Dominik Paździerz 242514
+
+using System.Web.Http.Cors;
+using System.Web.Http;
+using System.Collections.Generic;
+using System.Linq;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("section")]
+    [EnableCors(origins: "*", headers:"*" , methods:"*")]
+    public class SectionController : ApiController
+    {
+        static List<Section> sectionList = new List<Section>();
+
+        [HttpGet]
+        public IEnumerable<Section> Get()
+        {
+            return sectionList;
+        }
+
+        [HttpPost]
+        public Section Post(Section section)
+        {
+            var ids = sectionList.Select(x => x.Id);
+            section.Id = ids.Count() == 0 ? 0 : ids.Max() + 1;
+            sectionList.Add(section);
+            return section;
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(Section section)
+        {
+            var found = sectionList.Find(x => x.Id == section.Id);
+
+            if (found == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(found);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var indexToDelete = sectionList.FindIndex(t => t.Id == id);
+
+            if (indexToDelete == -1)
+            {
+                return NotFound();
+            }
+
+            sectionList.RemoveAt(indexToDelete);
+            return Ok();
+        }
+    }
+}
--- e-got-backend/eGOTBackend/Models/Section.cs
+++ e-got-backend/eGOTBackend/Models/Section.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace WebApplication2.Models
 {
+    [DataContract]
     public partial class Section
     {
         public Section()
@@ -10,13 +12,21 @@ namespace WebApplication2.Models
             Route = new HashSet<Route>();
         }
 
+        [DataMember(Name = "id")]
         public int Id { get; set; }
+        [DataMember(Name = "startLocation")]
         public int StartLocation { get; set; }
+        [DataMember(Name = "endLocation")]
         public int EndLocation { get; set; }
+        [DataMember(Name = "length")]
         public float Length { get; set; }
+        [DataMember(Name = "elevationGain")]
         public float ElevationGain { get; set; }
+        [DataMember(Name = "score")]
         public int Score { get; set; }
+        [DataMember(Name = "status")]
         public bool Status { get; set; }
+        [DataMember(Name = "mountainRange")]
         public int MountainRange { get; set; }
 
         public virtual Location EndLocationNavigation { get; set; }
