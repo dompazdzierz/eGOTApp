09510d4de706979ddc591b58fa002f0c9432a100
Karol Belina <karolbelina@gmail.com>
2020-01-23 02:24:10 +0100

    Add location controller

---
+++ e-got-backend/eGOTBackend/Controllers/LocationsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using eGOTBackend.Repostiories;
+using eGOTBackend.Models;
+
+namespace eGOTBackend.Controllers
+{
+    public class LocationsController : ApiController
+    {
+        private readonly LocationsRepository _locationsRepository = new LocationsRepository();
+
+        [HttpGet]
+        [ActionName("getAll")]
+        public List<Location> GetAll()
+        {
+            return _locationsRepository.GetAll();
+        }
+    }
+}
---
+++ e-got-backend/eGOTBackend/Repostiories/LocationsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using eGOTBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGOTBackend.Repostiories
+{
+    public class LocationsRepository : BaseRepository
+    {
+        public List<Location> GetAll()
+        {
+            var sections = dbContext.Location.ToList();
+
+            if (sections == null)
+            {
+                return null;
+            }
+
+            return sections;
+        }
+    }
+}
